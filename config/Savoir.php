7859fb58bc5b5847796dd5052702083093f4c0b2
JolanLar <jolanlargeteau@gmail.com>
2020-01-23 09:51:35 +0100

    Commit fin rtc

---
+++ config/Savoir.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Savoir extends Model {
+    
+    protected $table = 'savoir';
+    public $timestamps = false;
+
+}
+
+?>
