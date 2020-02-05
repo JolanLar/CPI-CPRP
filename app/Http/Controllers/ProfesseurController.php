cd80d60490da71c6cc4d8fd205acd382dbaa25af
EzONiks <paulos.ruben@gmail.com>
2020-02-05 11:20:03 +0100

    commit 11:19/05022020

--- app/Http/Controllers/ProfesseurController.php
+++ app/Http/Controllers/ProfesseurController.php
@@ -93,40 +93,29 @@ class ProfesseurController extends Controller
             }
             $nom = $dr->Nom;
             $prenom = $dr->Prenom;
+            $lesSavoirs = App\Savoir::select('savoirdetaille.idsavoir as idSavoir', 'savoir.libelleSavoir as libelleSavoir', 'savoirdetaille.idFiliere as idFiliere')->distinct()
+            ->join('savoirdetaille', 'savoirdetaille.idSavoir', '=', 'savoir.idSavoir')->orderBy('idSavoir', 'ASC')
+            ->get();;
+            $lesSavoirsDetaille = App\SavoirDetaille::select('idSavoirDetaille', 'titreSavoirDetaille', 'idSavoir', 'idFiliere')
+                ->distinct()
+                ->groupBy('idSavoirDetaille', 'titreSavoirDetaille', 'idSavoir', 'idFiliere')
+                ->get();;
+            $filieres = App\Filiere::all();
+            $competences = App\Competence::all();
+            $lesCompSavoirsDetaille = App\SavoirDetaille::select('idSavoirDetaille', 'idCompetence', 'idFiliere')->get();
+            $lesSousSavoirsDetaille = App\SousSavoirDetaille::select('idSousSavoirDetaille', 'titreSousSavoirDetaille',
+                'soussavoirdetaille.idSavoirDetaille', 'savoirdetaille.idSavoir')
+                ->join('savoirdetaille', 'savoirdetaille.idSavoirDetaille', '=', 'soussavoirdetaille.idSavoirDetaille')
+                ->where('soussavoirdetaille.idFiliere', 1)->distinct()->groupBy('idSousSavoirDetaille', 'titreSousSavoirDetaille', 'soussavoirdetaille.idSavoirDetaille', 'savoirdetaille.idSavoir')->get();;
+            $lesCompSousSavoirsDetaille = App\SousSavoirDetaille::select('idSousSavoirDetaille', 'idCompetence')->where('idFiliere', 1)->get();
 
-            // Récupération des informations - pour BTS CPI
-            $lesCompetencesCPI = App\Competence::where('idFiliere', 1)->get();
-            $lesSavoirsCPI = App\Savoir::select('savoirdetaille.idsavoir as idSavoir', 'savoir.libelleSavoir as libelleSavoir')->distinct()
-            ->join('savoirdetaille', 'savoirdetaille.idSavoir', '=', 'savoir.idSavoir')
-            ->where('idFiliere', 1)->get();
-            //tab savoirDetaille S1.1 ...
-            $lesSavoirsDetailleCPI = App\SavoirDetaille::select('idSavoirDetaille', 'titreSavoirDetaille', 'idSavoir')->where('idFiliere', 1)->distinct()->groupBy('idSavoirDetaille', 'titreSavoirDetaille', 'idSavoir')->get();
-            // compte le nb de compétences
-            $countCPI = App\Competence::where('idFiliere', 1)->count();
-            $lesCompSavoirsDetailleCPI = App\SavoirDetaille::select('idSavoirDetaille', 'idCompetence')->where('idFiliere', 1)->get();
-            // les sous-savoirsDetaille
-            $lesSousSavoirsDetailleCPI = App\SousSavoirDetaille::select('idSousSavoirDetaille', 'titreSousSavoirDetaille', 'soussavoirdetaille.idSavoirDetaille', 'savoirdetaille.idSavoir')->join('savoirdetaille', 'savoirdetaille.idSavoirDetaille', '=',
-                'soussavoirdetaille.idSavoirDetaille')
-                ->where('soussavoirdetaille.idFiliere', 1)->distinct()->groupBy('idSousSavoirDetaille', 'titreSousSavoirDetaille', 'soussavoirdetaille.idSavoirDetaille', 'savoirdetaille.idSavoir')->get();
-            $lesCompSousSavoirsDetailleCPI = App\SousSavoirDetaille::select('idSousSavoirDetaille', 'idCompetence')->where('idFiliere', 1)->get();
+            $nbCompetences = [];
+            foreach ($filieres as $uneFiliere) {
+                array_push($nbCompetences, App\Competence::where('idFiliere', '=', $uneFiliere->idFiliere)->count());
+            }
 
-            // Récupération des informations - pour BTS CPRP
-            $lesCompetencesCPRP = App\Competence::where('idFiliere', 2)->get();
-            $lesSavoirsCPRP = App\Savoir::all();
-            //tab sous savoirs S1.1 ...
-            $lesSavoirsDetailleCPRP = App\SavoirDetaille::select('idSavoirDetaille', 'titreSavoirDetaille', 'idSavoir')->where('idFiliere', 2)->distinct()->groupBy('idSavoirDetaille', 'titreSavoirDetaille', 'idSavoir')->get();
-            // compte le nb de compétences
-            $countCPRP = App\Competence::where('idFiliere', 2)->count();
-            $lesCompSavoirsDetailleCPRP = App\SavoirDetaille::select('idSavoirDetaille', 'idCompetence')->where('idFiliere', 2)->get();
-            // les sous-savoirsDetaille
-            $lesSousSavoirsDetailleCPRP = App\SousSavoirDetaille::select('idSousSavoirDetaille', 'titreSousSavoirDetaille', 'soussavoirdetaille.idSavoirDetaille', 'savoirdetaille.idSavoir')->join('savoirdetaille', 'savoirdetaille.idSavoirDetaille', '=',
-                'soussavoirdetaille.idSavoirDetaille')
-                ->where('soussavoirdetaille.idFiliere', 2)->distinct()->groupBy('idSousSavoirDetaille', 'titreSousSavoirDetaille', 'soussavoirdetaille.idSavoirDetaille', 'savoirdetaille.idSavoir')->get();
-            $lesCompSousSavoirsDetailleCPRP = App\SousSavoirDetaille::select('idSousSavoirDetaille', 'idCompetence')->where('idFiliere', 2)->get();
-
-            return view('professeur_rcs', compact('nom', 'prenom', 'lesCompetencesCPI', 'lesSavoirsCPI', 'lesSavoirsDetailleCPI',
-                'countCPI', 'lesCompSavoirsDetailleCPI', 'lesSousSavoirsDetailleCPI', 'lesCompSousSavoirsDetailleCPI' ,
-                'lesCompetencesCPRP', 'lesSavoirsCPRP', 'lesSavoirsDetailleCPRP', 'countCPRP', 'lesCompSavoirsDetailleCPRP', 'lesSousSavoirsDetailleCPRP', 'lesCompSousSavoirsDetailleCPRP'));
+            return view('professeur_rcs', compact('nom', 'prenom', 'filieres', 'competences', 'competences', 'nbCompetences', 'lesSavoirs',
+            'lesCompSavoirsDetaille', 'lesSousSavoirsDetaille', 'lesSavoirsDetaille', 'lesCompSousSavoirsDetaille'));
         }
         else
         {
--- resources/views/professeur_rcs.blade.php
+++ resources/views/professeur_rcs.blade.php
@@ -8,204 +8,99 @@
       </div>
       <form class="col-lg-12 text-center">
          <div class="col-lg-12 text-center">
-            <input type="radio" class="choixref" name="choixref2" value="cpi" checked>
-            <label for="cpi">CPI</label>
-            <input type="radio" class="choixref" name="choixref2" value="cprp">
-            <label for="cprp">CPRP</label>
+             @foreach($filieres as $i => $uneFiliere)
+                 @if($i==0)
+                     <input type="radio" class="choixref" name="choixref" value="{{$uneFiliere->libelleFiliere}}" checked>
+                 @else
+                     <input type="radio" class="choixref" name="choixref" value="{{$uneFiliere->libelleFiliere}}">
+                 @endif
+                 <label for="{{$uneFiliere->libelleFiliere}}">{{$uneFiliere->libelleFiliere}}</label>
+             @endforeach
          </div>
       </form>
-         <table class="table table-responsive" id="tabcpi" cellspacing="0" border="0">
-	<colgroup width="85"></colgroup>
-	<colgroup width="313"></colgroup>
-	<colgroup span="14" width="85"></colgroup>
-	<tr>
-		<td style="border-top: 2px solid ; border-bottom: 2px solid ; border-left: 2px solid ; border-right: 2px solid " colspan={{$countCPI + 2}} height="29"><b><font face="Calibri" size=5 >BTS CPI : Relations principales entre les comp&eacute;tences et les savoirs</font></b></td>
-	</tr>
-	<tr>
-		<td style="border-left: 2px solid ; border-right: 1px solid " colspan=2 height="18" >COMPETENCES BTS CPI</td>
-
-		{{-- affiche l'ensemble des compétences pour la filière CPi --}}
-        @foreach($lesCompetencesCPI as $key => $uneCompetence)
-
-            @if ($key > 3)
-                <td id="comp{{$key}}" bgcolor="#EBF1DE"><b>{{ $uneCompetence->codeCompetence }}</b></td>
-            @else
-                <td id="comp{{$key}}" bgcolor="#F2DCDB"><b>{{ $uneCompetence->codeCompetence }}</b></td>
-            @endif
-
-
-		@endforeach
-
-    </tr>
-
-	{{-- boucle sur le tableau lesSavoir --}}
-    @foreach ($lesSavoirsCPI as $unSavoir)
-    {{-- les savoirs --}}
-        <tr>
-            <td style="border-top: 2px solid ; border-bottom: 2px solid ; border-left: 2px solid ; border-right: 2px solid " colspan=20 height="18" align="left"  bgcolor="#D9D9D9"><font face="Calibri">
-                S{{ $unSavoir->idSavoir }} - {{ $unSavoir->libelleSavoir }}
-            </font></td>
-        </tr>
-
-    {{-- les sous-savoirs pour chaque Savoir--}}
-        @foreach ($lesSavoirsDetailleCPI as $unSavoirDetaille)
-			{{-- si les idSavoir correspondent ajoute les sous savoir --}}
-            @if ($unSavoirDetaille->idSavoir == $unSavoir->idSavoir)
-                <tr>
-                    <td style="border-bottom: 1px solid ; border-left: 2px solid ; border-right: 1px solid " height="18" align="center" >
-                        <font face="Calibri">S{{$unSavoirDetaille->idSavoirDetaille}}</font></td>
-                    <td align="left" ><font face="Calibri">{{$unSavoirDetaille->titreSavoirDetaille}}</font></td>
-
-                {{-- ajoute les colonnes pour les compétences --}}
-
-                @for ($i = 0; $i < $countCPI; $i++)
-                    @php ($test = false)
-
-                {{-- pour chaque savoirdetaille --}}
-                    @foreach ($lesCompSavoirsDetailleCPI as $cpSS)
-
-                        @if ($cpSS->idSavoirDetaille == $unSavoirDetaille->idSavoirDetaille &&$cpSS->idCompetence == $i + 1)
-                            <td align="center"><b>x</b></td>
-                            @php ($test = true)
+          {{ var_dump($nbCompetences) }}
+       @foreach($filieres as $uneFiliere)
+            <table class="table table-responsive" id="tab{{ strtolower($uneFiliere->libelleFiliere) }}" cellspacing="0" border="0">
+            <colgroup width="85"></colgroup>
+            <colgroup width="313"></colgroup>
+            <colgroup span="14" width="85"></colgroup>
+            <tr>
+                <td style="border-top: 2px solid ; border-bottom: 2px solid ; border-left: 2px solid ; border-right: 2px solid " colspan={{ $nbCompetences[$uneFiliere->idFiliere -1]  + 2}} height="29"><b><font face="Calibri" size=5 >BTS {{ strtoupper($uneFiliere->libelleFiliere) }} : Relations principales entre les comp&eacute;tences et les savoirs</font></b></td>
+            </tr>
+            <tr>
+                <td style="border-left: 2px solid ; border-right: 1px solid " colspan=2 height="18" >COMPETENCES BTS {{$uneFiliere->libelleFiliere}}</td>
+                    @foreach($competences as $key => $uneCompetence)
+                        @if($uneCompetence->idFiliere == $uneFiliere->idFiliere)
+                            @if ($key > 3)
+                                <td id="comp{{$key}}" bgcolor="#EBF1DE"><b>{{ $uneCompetence->codeCompetence }}</b></td>
+                            @else
+                                <td id="comp{{$key}}" bgcolor="#F2DCDB"><b>{{ $uneCompetence->codeCompetence }}</b></td>
+                            @endif
                         @endif
-
                     @endforeach
+            </tr>
+                     @foreach($lesSavoirs as $unSavoir)
+                         @if($unSavoir->idFiliere == $uneFiliere->idFiliere)
+                             <tr>
+                                 <td style="border-top: 2px solid ; border-bottom: 2px solid ; border-left: 2px solid ; border-right: 2px solid " colspan=20 height="18" align="left"  bgcolor="#D9D9D9"><font face="Calibri">
+                                         S{{ $unSavoir->idSavoir }} - {{ $unSavoir->libelleSavoir }}
+                                     </font></td>
+                             </tr>
+                             @foreach ($lesSavoirsDetaille as $unSavoirDetaille)
+                                 {{-- si les idSavoir correspondent ajoute les sous savoir --}}
+                                 @if ($unSavoirDetaille->idSavoir == $unSavoir->idSavoir && $unSavoirDetaille->idFiliere == $uneFiliere->idFiliere && $unSavoir->idFiliere == $uneFiliere->idFiliere)
+                                     <tr>
+                                         <td style="border-bottom: 1px solid ; border-left: 2px solid ; border-right: 1px solid " height="18" align="center" >
+                                             <font face="Calibri">S{{$unSavoirDetaille->idSavoirDetaille}}</font></td>
+                                         <td align="left" ><font face="Calibri">{{$unSavoirDetaille->titreSavoirDetaille}}</font></td>
 
-                    @if ($test == false)
-                        <td><br></td>
-                    @endif
+                                         {{-- ajoute les colonnes pour les compétences --}}
 
-                @endfor
-				</tr>
+                                         @for ($i = 0; $i < $nbCompetences[$uneFiliere->idFiliere - 1]; $i++)
+                                             @php ($test = false)
+                                             {{-- pour chaque savoirdetaille --}}
+                                             @foreach ($lesCompSavoirsDetaille as $cpSS)
+                                                 @if ($cpSS->idSavoirDetaille == $unSavoirDetaille->idSavoirDetaille && $cpSS->idCompetence == $i + 1 && $cpSS->idFiliere == $uneFiliere->idFiliere)
+                                                     <td align="center"><b>x</b></td>
+                                                     @php ($test = true)
+                                                 @endif
+                                             @endforeach
+                                             @if ($test == false)
+                                                 <td><br></td>
+                                             @endif
+                                         @endfor
+                                     </tr>
 
-                 {{-- les sous-savoirsDetaille --}}
-                 @foreach($lesSousSavoirsDetailleCPI as $unSousSavoirDetaille)
+                                     {{-- les sous-savoirsDetaille --}}
+                                     @foreach($lesSousSavoirsDetaille as $unSousSavoirDetaille)
+                                         @if($unSousSavoirDetaille->idSavoir == $unSavoir->idSavoir && $unSousSavoirDetaille->idSavoirDetaille == $unSavoirDetaille->idSavoirDetaille)
+                                             <tr>
+                                                 <td style="border-bottom: 1px solid ; border-left: 2px solid ; border-right: 1px solid " height="18"
+                                                     align="center" ><font face="Calibri">S{{$unSousSavoirDetaille->idSousSavoirDetaille}}</font></td>
+                                                 <td align="left" ><font face="Calibri">{{$unSousSavoirDetaille->titreSousSavoirDetaille}}</font></td>
 
-                     @if($unSousSavoirDetaille->idSavoir == $unSavoir->idSavoir && $unSousSavoirDetaille->idSavoirDetaille == $unSavoirDetaille->idSavoirDetaille)
-                         <tr>
-                             <td style="border-bottom: 1px solid ; border-left: 2px solid ; border-right: 1px solid " height="18"
-                                 align="center" ><font face="Calibri">S{{$unSousSavoirDetaille->idSousSavoirDetaille}}</font></td>
-                             <td align="left" ><font face="Calibri">{{$unSousSavoirDetaille->titreSousSavoirDetaille}}</font></td>
+                                                 @for ($i = 0; $i < $nbCompetences[$uneFiliere->idFiliere-1]; $i++)
+                                                     @php ($test = false)
 
-                             @for ($i = 0; $i < $countCPI; $i++)
-                                 @php ($test = false)
-
-                                 @foreach ($lesCompSousSavoirsDetailleCPI as $cpSSD)
-                                     @if ($cpSSD->idSousSavoirDetaille == $unSousSavoirDetaille->idSousSavoirDetaille && $cpSSD->idCompetence == $i + 1)
-                                         <td align="center"><b>x</b></td>
-                                         @php ($test = true)
-                                     @endif
-                                 @endforeach
-
-                                 @if ($test == false)
-                                     <td><br></td>
+                                                     @foreach ($lesCompSousSavoirsDetaille as $cpSSD)
+                                                         @if ($cpSSD->idSousSavoirDetaille == $unSousSavoirDetaille->idSousSavoirDetaille && $cpSSD->idCompetence == $i + 1)
+                                                             <td align="center"><b>x</b></td>
+                                                             @php ($test = true)
+                                                         @endif
+                                                     @endforeach
+                                                     @if ($test == false)
+                                                         <td><br></td>
+                                                     @endif
+                                                 @endfor
+                                             </tr>
+                                         @endif
+                                     @endforeach
                                  @endif
-                             @endfor
-                         </tr>
-                     @endif
-                 @endforeach
-            @endif
+                             @endforeach
+                         @endif
+                     @endforeach
         @endforeach
-
-    @endforeach
-
-</table>
-
-{{-- Autre tableau --}}
-          <table class="table table-responsive" id="tabcprp" cellspacing="0" border="0">
-	<colgroup width="85"></colgroup>
-	<colgroup width="647"></colgroup>
-	<colgroup span="18" width="85"></colgroup>
-	<tr>
-		<td style="border-top: 2px solid ; border-bottom: 2px solid ; border-left: 2px solid ; border-right: 2px solid " colspan={{$countCPRP + 2}} height="29" align="center" ><b><font face="Calibri" size=5>BTS CPRP : Relations principales entre les compétences et les savoirs</font></b></td>
-	</tr>
-	<tr>
-		<td style="border-left: 2px solid ; border-right: 1px solid " colspan=2 height="18" align="center" >COMPETENCES BTS CPRP</td>
-        {{-- affiche l'ensemble des compétences pour la filière CPRP --}}
-        @foreach($lesCompetencesCPRP as $key => $uneCompetence)
-
-            @if ($key > 3)
-                <td id="comp{{$key}}" bgcolor="#EBF1DE"><b>{{ $uneCompetence->codeCompetence }}</b></td>
-            @else
-                <td id="comp{{$key}}" bgcolor="#F2DCDB"><b>{{ $uneCompetence->codeCompetence }}</b></td>
-            @endif
-
-
-        @endforeach
-
-    </tr>
-
-      {{-- boucle sur le tableau lesSavoir --}}
-      @foreach ($lesSavoirsCPRP as $unSavoir)
-          {{-- les savoirs --}}
-          <tr>
-              <td style="border-top: 2px solid ; border-bottom: 2px solid ; border-left: 2px solid ; border-right: 2px solid " colspan=20 height="18" align="left"  bgcolor="#D9D9D9"><font face="Calibri">
-                      S{{ $unSavoir->idSavoir }} - {{ $unSavoir->libelleSavoir }}
-                  </font></td>
-          </tr>
-
-          {{-- les sous-savoirs pour chaque Savoir--}}
-          @foreach ($lesSavoirsDetailleCPRP as $unSavoirDetaille)
-              {{-- si les idSavoir correspondent ajoute les sous savoir --}}
-              @if ($unSavoirDetaille->idSavoir == $unSavoir->idSavoir)
-                  <tr>
-                      <td style="border-bottom: 1px solid ; border-left: 2px solid ; border-right: 1px solid " height="18" align="center" >
-                          <font face="Calibri">S{{$unSavoirDetaille->idSavoirDetaille}}</font></td>
-                      <td align="left" ><font face="Calibri">{{$unSavoirDetaille->titreSavoirDetaille}}</font></td>
-
-                      {{-- ajoute les colonnes pour les compétences --}}
-
-                      @for ($i = 0; $i < $countCPRP; $i++)
-                          @php ($test = false)
-
-                          {{-- pour chaque savoirdetaille --}}
-                          @foreach ($lesCompSavoirsDetailleCPRP as $cpSS)
-
-                              @if ($cpSS->idSavoirDetaille == $unSavoirDetaille->idSavoirDetaille &&$cpSS->idCompetence == $i + 1)
-                                  <td align="center"><b>x</b></td>
-                                  @php ($test = true)
-                              @endif
-
-                          @endforeach
-
-                          @if ($test == false)
-                              <td><br></td>
-                          @endif
-
-                      @endfor
-                  </tr>
-
-                  {{-- les sous-savoirsDetaille --}}
-                  @foreach($lesSousSavoirsDetailleCPRP as $unSousSavoirDetaille)
-
-                      @if($unSousSavoirDetaille->idSavoir == $unSavoir->idSavoir && $unSousSavoirDetaille->idSavoirDetaille == $unSavoirDetaille->idSavoirDetaille)
-                          <tr>
-                              <td style="border-bottom: 1px solid ; border-left: 2px solid ; border-right: 1px solid " height="18"
-                                  align="center" ><font face="Calibri">S{{$unSousSavoirDetaille->idSousSavoirDetaille}}</font></td>
-                              <td align="left" ><font face="Calibri">{{$unSousSavoirDetaille->titreSousSavoirDetaille}}</font></td>
-
-                              @for ($i = 0; $i < $countCPI; $i++)
-                                  @php ($test = false)
-
-                                  @foreach ($lesCompSousSavoirsDetailleCPRP as $cpSSD)
-                                      @if ($cpSSD->idSousSavoirDetaille == $unSousSavoirDetaille->idSousSavoirDetaille && $cpSSD->idCompetence == $i + 1)
-                                          <td align="center"><b>x</b></td>
-                                          @php ($test = true)
-                                      @endif
-                                  @endforeach
-
-                                  @if ($test == false)
-                                      <td><br></td>
-                                  @endif
-                              @endfor
-                          </tr>
-                      @endif
-                  @endforeach
-              @endif
-          @endforeach
-
-      @endforeach
-</table>
+        </table>
 
       </div>
    </div>
