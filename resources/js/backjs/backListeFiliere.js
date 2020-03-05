85af3ab59a2e219fc2c8c4e4690d83c0419a13f9
JolanLar <jolanlargeteau@gmail.com>
2020-03-05 15:21:35 +0100

    25/02/2020

--- resources/js/backjs/backListeFiliere.js
+++ resources/js/backjs/backListeFiliere.js
@@ -1,37 +1,54 @@
 $(document).on('click', '.filiereEdit', function () {
     var value = $('#filiere-' + $(this).attr('id')).text().toString().toUpperCase();
     var idFiliere = $(this).attr('id');
-    $('#filiere-' + $(this).attr('id')).html('<input id="filiereText-' + $(this).attr('id') + '" type="text" value="' + value + '">');
+    $('#filiere-' + $(this).attr('id')).html('<input id="filiereText-' + $(this).attr('id') + '" type="text" value="' + value + '" />');
     $('#filiereText-' + $(this).attr('id')).select();
-    $('#filiereText-' + $(this).attr('id')).focusout(function () {
+});
+
+
+$(document).on('keyup', 'input', function (e) {
+    if (e.keyCode === 27) {
+        window.location.replace("/public/gestionfiliere");
+    }
+});
+
+$(document).on('focusout', 'input', function () {
+    const idInput = $(this).attr('id');
+    if(idInput) {
+        const idInputSplit = idInput.split('-');
+        const idFiliere = idInputSplit[1];
         var value = $(this).val().toUpperCase();
         $('#filiere-' + idFiliere).html(value);
-        data = { idFiliere, idFiliere, libelle: value };
+        data = {idFiliere, idFiliere, libelle: value};
+        $.ajax({
+            type: "POST",
+            url: 'gestionfiliere/creation',
+            data: data,
+            headers:
+                {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+            success: function () {
+                message('success', 'Filière ajoutée !');
+            }
+        });
         $.ajax({
             type: "POST",
             url: "gestionfiliere/edit",
             data: data,
             headers:
-            {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
+                {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
         });
-    });
-    $('#filiereText-' + $(this).attr('id')).on('keypress', function (e) {
-        if (e.which == 13) {
-            $(this).blur();
-        }
-    });
-    $('#filiereText-' + $(this).attr('id')).keyup(function (e) {
-        if (e.keyCode === 27) $('#filiere-' + idFiliere).html(value);
-    });
+    }
 });
 
 $(document).on('click', '.filiereDelete', function () {
     var idFiliere = $(this).attr('id');
     var libelleFiliere = $('#filiere-'+idFiliere).text();
     var supprimer = confirm("Voulez-vous supprimer la filière : "+libelleFiliere+" ?\rAttention cela supprimera toutes les données liées à cette filière !\r\r!!!!!!!!!!!!!!!!!!!!!!!\r Les classes et les élèves ne seront pas supprimé, si vous souhaitez les supprimer rendez-vous à la page: Gestion des classes \r!!!!!!!!!!!!!!!!!!!!!!!");
-    $('#filiere-'+idFiliere).parent().hide();
+    $('#filiere-'+idFiliere).parent().remove();
     if (supprimer) {
         var data = { idFiliere: idFiliere };
         $.ajax({
@@ -72,43 +89,12 @@ $(document).on('click', '#ajoutFiliere', function () {
         }
     });
     function affiche(idFiliere) {
-        var value = "Filière";
-        $('#laTable').append('<tr><td width="85%" class="libelleFiliere" id="filiere-' + idFiliere + '"><input id="filiereText-' + idFiliere + '" type="text" value="' + value + '"></td><td><a class="filiereEdit" id="' + idFiliere + '" href="javascript: void(0"><i class="fas fa-pencil-alt"></i></a></td><td><a class="filiereDelete text-danger" id="' + idFiliere + '" href="javascript: void(0"><i class="fas fa-trash-alt"></i></a></td></tr>');
+        $('#laTable').append('<tr><td width="85%" class="libelleFiliere" id="filiere-' + idFiliere + '"><input id="filiereText-' + idFiliere + '" type="text" value="" /></td><td><a class="filiereEdit" id="' + idFiliere + '" href="javascript: void(0"><i class="fas fa-pencil-alt"></i></a></td><td><a class="filiereDelete text-danger" id="' + idFiliere + '" href="javascript: void(0"><i class="fas fa-trash-alt"></i></a></td></tr>');
         $('#filiereText-' + idFiliere).select();
-        $('#filiereText-' + idFiliere).focusout(function () {
-            ajout(idFiliere, $('#filiereText-' + idFiliere).val());
-        });
-        $('#filiereText-' + idFiliere).keyup(function (e) {
-            if (e.keyCode === 13) {
-                $('#filiereText-' + idFiliere).blur();
-            }
-            if (e.keyCode === 27) {
-                window.location.replace("/public/gestionfiliere");
-            }
-        });
-    }
-    function ajout(idFiliere, libelle) {
-        libelle = libelle.toString().toUpperCase();
-        setTimeout(function(){
-            $('#filiere-' + idFiliere).html(libelle);
-        }, 100);
-        data = { id: idFiliere, libelle: libelle };
-        $.ajax({
-            type: "POST",
-            url: 'gestionfiliere/creation',
-            data: data,
-            headers:
-            {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
-            success: function () {
-                message('success', 'Filière ajoutée !');
-            }
-        });
     }
 });
 
 $(document).on('dblclick', '.libelleFiliere', function() {
     var id = $(this).attr('id').split('-');
-    $('#'+id[1]).click();
+    $('#'+id[1]).trigger('click');
 });
